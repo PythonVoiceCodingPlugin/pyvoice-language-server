53c9ccaf58c47007e786dbc338482df70624347c
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2022-01-31 16:43:24 +0200

    if you more chances

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,12 @@ from datetime import datetime
 project = "pyvoice"
 author = "mpourmpoulis"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_click"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx_click",
+    "sphinx_rtd_theme",
+]
 autodoc_typehints = "description"
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
