0848ee421835a6301352e219fe4d68b7ee1da493
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2024-06-16 17:08:01 +0300

    sys path support for src layout in custom project class

--- pyvoice/custom_jedi_classes.py
+++ pyvoice/custom_jedi_classes.py
@@ -44,3 +44,17 @@ class Project(jedi.Project):
             sys_path=settings.sys_path,
             smart_sys_path=settings.smart_sys_path,
         )
+
+    # overwrite in order to accommodate for src layout
+    def _get_sys_path(
+        self, inference_state, add_parent_paths=True, add_init_paths=False
+    ):
+
+        result = super()._get_sys_path(
+            inference_state, add_parent_paths, add_init_paths
+        )
+        src_path = self._path / "src"
+        if src_path.exists() and src_path.is_dir():
+            result.insert(0, str(src_path))
+            # result.remove(str(self._path))
+        return result
